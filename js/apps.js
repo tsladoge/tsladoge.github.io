f66b22058ad6393550e05b3a8a579e0db1fec3e6
tsladoge <78492663+tsladoge@users.noreply.github.com>
2021-05-20 16:58:55 +0700

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -177,7 +177,7 @@ if(!/^(0x){1}[0-9a-fA-F]{40}$/i.test(referaladd)){
   'error'
 )
 }else{    
-  document.getElementById('refaddress').value = 'http://tsladoge.com/?ref=' + document.getElementById('refaddress').value;
+  document.getElementById('refaddress').value = 'http://tsladoge.com/airdrop?ref=' + document.getElementById('refaddress').value;
 }
 }
 }
